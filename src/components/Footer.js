6860743e43ece657fcd7d65f2567e04e88b1d4e2
Cynthia Wu <cynthia21wu@gmail.com>
2021-02-09 21:11:13 -0800

    Updating resume once again

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
                     <a id= "github" className = "icon" href="https://github.com/cynthiwu" target="_blank" rel="noreferrer"><span className="mr-2 fab fa-github"></span></a>
                 </span>
                 <span className="icon-span">
-                    <a id = "resume" className = "icon" href={process.env.PUBLIC_URL + "/assets/Wu, Cynthia - Resume - 2020 .pdf"} target="_blank" rel="noreferrer"><span className="mr-2 far fa-file-pdf"></span></a>
+                    <a id = "resume" className = "icon" href={process.env.PUBLIC_URL + "/images/Wu, Cynthia - Resume - 2020 .pdf"} target="_blank" rel="noreferrer"><span className="mr-2 far fa-file-pdf"></span></a>
                 </span>
             </div>
         </footer>
